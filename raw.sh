32796db1bbf0491e36f8252afd15a548a962ba01
Luz <779410241@qq.com>
2022-05-27 16:26:40 +0800

    Update raw.sh

--- raw.sh
+++ raw.sh
@@ -81,7 +81,7 @@ awk '{if($1=="chr1") print $0}' P1_1.5Sham.counts > P1_1.5Sham_chr1.counts
 ./aggregateSignal mm10_100kb.txt ~/ref_genome/Mus_musculus/mm10.fa.fai P1_1.5Sham_chr1.counts P1_1.5Sham_chr1.txt
 
 ./genDatasetsRH P1_1.5Sham_chr1_counts_pairs.tab 1000000 5 pairwise featurefiles.txt no out/ yes Window
-## directory out must be created before running genDatasetsRH!!!!!!!!!!!!!!!!!!!!!!!!!!!
+## !!!!!!!!!!!!!!!!!!!directory out must be created before running genDatasetsRH!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 grep chr17 Gm12878_RawData_5000bp_seqdepth_norm_H3k4me1.txt | cut -d _ -f 1,2 > tmp_1.txt
@@ -96,6 +96,8 @@ paste -d "" tmp_1.txt tmp_2.txt > H3k4me2_chr17.txt
 rm tmp_1.txt tmp_2.txt
 sed -i '1 i Gene\tH3k4me2' H3k4me2_chr17.txt
 
+
+## Makefile (for genDatasetsRH)
 SRC=Dataset.C Distance.C Framework.C
 #INCLPATH1=common
 LIBPATH = ~/softwares/gsl/lib
@@ -108,8 +110,6 @@ CC=g++
 CFLAGS = -g -std=c++0x
 LFLAG = -lgsl -lgslcblas 
 
-
-## Makefile (for genDatasetsRH)
 #local: $(SRC)
 #	$(CC) $(SRC) -I $(INCLPATH1) -I $(LOCINCL)  -L $(LOCLIB) $(LFLAG) $(CFLAGS) -o regTreeDC
 
@@ -122,5 +122,28 @@ clean:
 ## debug orders
 #splitPairs -> splitRegionsGenPairs        -> showFeaturesPair
 #              generateFeatureFiles_Concat -> showFeatures
+
+
+
+bedtools makewindows -g ~/ref_genome/chrom_sizes/mm10.chrom.sizes -w 10000 > mm10_100kb.bed
+awk '{print $1 "\t" "CONVERT" "\t" "gene" "\t" $2 "\t" $3-1 "\t" "." "\t" "+" "\t" "." "\t" $1"_"$2"_"($3-1)}' mm10_100kb.bed > mm10_100kb.txt
+time bamCoverage -p 50 -of bedgraph --binSize 1 --normalizeUsing None -b ~/ChIP_seq/mouse/P1_heart/H3K27ac/P1_1.5Sham_uniquely_rm.bam -o P1_1.5Sham.bedgraph
+awk '{if($4!=0) print("chr"$0)}' P1_1.5Sham.bedgraph > P1_1.5Sham.counts
+
+awk '{if($1=="chr1") print $0}' P1_1.5Sham.counts > P1_1.5Sham_chr1.counts
+./aggregateSignal mm10_100kb.txt ~/ref_genome/Mus_musculus/mm10.fa.fai P1_1.5Sham_chr1.counts P1_1.5Sham_chr1.txt
+java -jar ~/softwares/juicer_tools_1.19.02.jar dump observed KR ~/C_data/Hi_C/mouse/heart/newborn.allValidPairs.hic 1 1 BP 100000 | sed '1d' |\
+awk '{if($1!=$2) print "chr1_"$1"_"($1+100000) "\t" "chr1_"$2"_"($2+100000) "\t" $3}' > P1_1.5Sham_chr1_counts_pairs.tab
+
+./genDatasetsRH P1_1.5Sham_chr1_counts_pairs.tab 1000000 5 regionwise featurefiles.txt no out/ yes Window  ## not use pairwise
+./regForest -t out/train0.txt -o out/ -k1 -l10 -n20 -b prior_window.txt -d out/test0.txt
+
+
+head -n 1 train0.txt > train0_tmp.txt
+sed '1d' train0.txt | awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7*10}' >> train0_tmp.txt
+head -n 1 test0.txt > test0_tmp.txt
+sed '1d' test0.txt | awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7*10}' >> test0_tmp.txt
+
+
 
 
