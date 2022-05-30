958b7605fa92015915b142e06124342f9eab29fd
Luz <779410241@qq.com>
2022-05-30 12:51:58 +0800

    Update zebrafish_HiC.sh

--- zebrafish_HiC.sh
+++ zebrafish_HiC.sh
@@ -1,1 +1,3 @@
-
+## no heart Hi-C data, replace with muscle (from "A map of cis-regulatory elements and 3D genome structures in zebrafish")
+## GEO accession: GSE134055
+## hic file url: https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM4662nnn/GSM4662083/suppl/GSM4662083_YueLab-HiC-Muscle_merged.hic
