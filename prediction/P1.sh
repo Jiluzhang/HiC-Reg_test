1d086bde9c4225729fe7b9973caf44880e157eac
Luz <779410241@qq.com>
2022-06-05 22:07:14 +0800

    Update P1.sh

--- prediction/P1.sh
+++ prediction/P1.sh
@@ -11,3 +11,15 @@ awk '{printf($1"\t");printf("%d\t", ($2+$3)*0.5/10000-10);printf("%d\t", ($2+$3)
      non_IREs_random_2000.bed > non_IREs_random_2000_100kb.bed
 bedtools intersect -a non_IREs_random_2000_100kb.bed -b P1_1.5Sham/obs_kr.txt -wa -wb -F 1 > P1_1.5Sham_non_IREs.txt  # ~2 min
 bedtools intersect -a non_IREs_random_2000_100kb.bed -b P1_1.5MI/obs_kr.txt -wa -wb -F 1 > P1_1.5MI_non_IREs.txt
+
+
+## generate obs for IRE-gene pairs
+awk '{printf("chr"$1 "\t");printf("%d\t", $2/10000);printf("%d\n", $5/10000)}' \
+      ~/genome_biology_2/mouse_IREs_gene_expression/RREs_Genes.txt | \
+awk '{if($2>$3) print($1 "\t" $3 "\t" $2);else if($2<$3) print$0}' > IREs_genes_bin.txt  # 1535
+
+## P1_1.5Sham
+bedtools intersect -a IREs_genes_bin.txt -b P1_1.5Sham/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5Sham_IREs_genes.txt  # 1268
+
+## P1_1.5MI
+bedtools intersect -a IREs_genes_bin.txt -b P1_1.5MI/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5MI_IREs_genes.txt  # 1268
