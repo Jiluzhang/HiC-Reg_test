bd45360b9028e79294745e5389bd6b4002d1da4c
Luz <779410241@qq.com>
2022-06-05 22:37:07 +0800

    Update P1.sh

--- prediction/P1.sh
+++ prediction/P1.sh
@@ -18,8 +18,15 @@ awk '{printf("chr"$1 "\t");printf("%d\t", $2/10000);printf("%d\n", $5/10000)}' \
       ~/genome_biology_2/mouse_IREs_gene_expression/RREs_Genes.txt | \
 awk '{if($2>$3) print($1 "\t" $3 "\t" $2);else if($2<$3) print$0}' > IREs_genes_bin.txt  # 1535
 
-## P1_1.5Sham
 bedtools intersect -a IREs_genes_bin.txt -b P1_1.5Sham/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5Sham_IREs_genes.txt  # 1268
-
-## P1_1.5MI
 bedtools intersect -a IREs_genes_bin.txt -b P1_1.5MI/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5MI_IREs_genes.txt  # 1268
+
+
+## generate obs for non_IRE-gene pairs
+awk '{printf($1"\t");printf("%d\t", ($2+$3)/2);printf("%d\n", ($2+$3)/2+1)}' non_IREs_random_2000.bed > non_IREs_random_2000_center.bed
+awk '{print "chr"$0}' ~/genome_biology/mouse/Genes_TSS.bed | bedtools closest -a non_IREs_random_2000_center.bed -b stdin > non_IREs_genes.txt
+awk '{printf($1 "\t");printf("%d\t", $2/10000);printf("%d\n", $5/10000)}' non_IREs_genes.txt | \
+awk '{if($2>$3) print($1 "\t" $3 "\t" $2);else if($2<$3) print$0}' > non_IREs_genes_bin.txt  # 1723
+
+bedtools intersect -a non_IREs_genes_bin.txt -b P1_1.5Sham/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5Sham_non_IREs_genes.txt  # 1650
+bedtools intersect -a non_IREs_genes_bin.txt -b P1_1.5MI/obs_kr.txt -wa -wb -f 1 -r | uniq > P1_1.5MI_non_IREs_genes.txt  # 1650
