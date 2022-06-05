eabb4824ed6326b48f0f80c54218faeb06ffce78
Luz <779410241@qq.com>
2022-06-06 00:34:32 +0800

    Update P8.sh

--- prediction/P8.sh
+++ prediction/P8.sh
@@ -6,3 +6,13 @@ bedtools intersect -a IREs_100kb.bed -b P8_1.5MI/obs_kr.txt -wa -wb -F 1 > P8_1.
 ## P8_1.5MI
 bedtools intersect -a non_IREs_random_2000_100kb.bed -b P8_1.5Sham/obs_kr.txt -wa -wb -F 1 > P8_1.5Sham_non_IREs.txt  # ~2 min
 bedtools intersect -a non_IREs_random_2000_100kb.bed -b P8_1.5MI/obs_kr.txt -wa -wb -F 1 > P8_1.5MI_non_IREs.txt
+
+
+## generate obs for IRE-gene pairs
+bedtools intersect -a IREs_genes_bin.txt -b P8_1.5Sham/obs_kr.txt -wa -wb -f 1 -r | uniq > P8_1.5Sham_IREs_genes.txt  # 1268
+bedtools intersect -a IREs_genes_bin.txt -b P8_1.5MI/obs_kr.txt -wa -wb -f 1 -r | uniq > P8_1.5MI_IREs_genes.txt  # 1268
+
+
+## generate obs for non_IRE-gene pairs
+bedtools intersect -a non_IREs_genes_bin.txt -b P8_1.5Sham/obs_kr.txt -wa -wb -f 1 -r | uniq > P8_1.5Sham_non_IREs_genes.txt  # 1650
+bedtools intersect -a non_IREs_genes_bin.txt -b P8_1.5MI/obs_kr.txt -wa -wb -f 1 -r | uniq > P8_1.5MI_non_IREs_genes.txt  # 1650
