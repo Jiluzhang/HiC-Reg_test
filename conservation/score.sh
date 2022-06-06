820a35d676433afc16de98912b0b4b60ca350549
Luz <779410241@qq.com>
2022-06-06 17:12:25 +0800

    Create score.sh

---
+++ conservation/score.sh
@@ -0,0 +1,2 @@
+wget -c http://hgdownload.soe.ucsc.edu/goldenPath/mm10/phastCons60way/mm10.60way.phastCons.bw
+wget -c http://hgdownload.soe.ucsc.edu/goldenPath/mm10/phyloP60way/mm10.60way.phyloP60way.bw
