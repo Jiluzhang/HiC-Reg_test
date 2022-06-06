6672e8461b189ac840e1297fb3f1ed0c18fbf6a7
Luz <779410241@qq.com>
2022-06-06 17:57:58 +0800

    Update score.sh

--- conservation/score.sh
+++ conservation/score.sh
@@ -1,2 +1,11 @@
+## phastCons
 wget -c http://hgdownload.soe.ucsc.edu/goldenPath/mm10/phastCons60way/mm10.60way.phastCons.bw
+computeMatrix reference-point --referencePoint center -p 40 -S mm10.60way.phastCons.bw \
+              -R RREs.bed non_IREs_random_2000.bed -o phastCons.gz -a 500 -b 500 -bs 5
+plotProfile -m phastCons.gz --yMin 0 --yMax 0.3 --dpi 600 -out phastCons.pdf
+
+## phyloP60way
 wget -c http://hgdownload.soe.ucsc.edu/goldenPath/mm10/phyloP60way/mm10.60way.phyloP60way.bw
+computeMatrix reference-point --referencePoint center -p 40 -S mm10.60way.phyloP60way.bw \
+              -R RREs.bed non_IREs_random_2000.bed -o phyloP60way.gz -a 500 -b 500 -bs 5
+plotProfile -m phyloP60way.gz --yMin 0 --yMax 0.3 --dpi 600 -out phyloP60way.pdf
