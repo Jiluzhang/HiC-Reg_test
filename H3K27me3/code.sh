46067443ea6e8fcd66754021e48002434174c48e
Luz <779410241@qq.com>
2022-06-06 12:57:51 +0800

    Update code.sh

--- H3K27me3/code.sh
+++ H3K27me3/code.sh
@@ -1,7 +1,22 @@
 ## P1 1.5Sham
 bamCompare -b1 ~/ChIP_seq/mouse/P1_heart/H3K27me3/P1_1.5Sham.bam -b2 ~/ChIP_seq/mouse/P1_heart/H3K27ac/P1_1.5Sham_input.bam \
-           --binSize 1 -p max -of bigwig -o P1_1.5Sham_log2ratio.bw
+           --binSize 1 -p max -of bigwig -o P1_1.5Sham_log2ratio.bw  # ~20min
+
 ## P1 1.5MI
-samtools merge -@ 50 H3K27ac.bam rep1/H3K27ac_WT_rep1_uniquely_rm.bam rep2/H3K27ac_WT_rep2_uniquely_rm.bam
-samtools index -@ 50 H3K27ac.bam
-bamCoverage -p 50 -of bedgraph --binSize 1 --normalizeUsing None -b H3K27ac.bam -o H3K27ac.bedgraph
+samtools merge -@ 50 P1_1.5MI.bam P1_1.5MI_rep1/P1_1.5MI_rep1_uniquely_rm.bam P1_1.5MI_rep2/P1_1.5MI_rep2_uniquely_rm.bam
+samtools index -@ 50 P1_1.5MI.bam
+bamCompare -b1 ~/ChIP_seq/mouse/P1_heart/H3K27me3/P1_1.5MI.bam -b2 ~/ChIP_seq/mouse/P1_heart/H3K27ac/P1_1.5MI_input.bam \
+           --binSize 1 -p max -of bigwig -o P1_1.5MI_log2ratio.bw
+
+## P8 1.5Sham
+samtools merge -@ 50 P8_1.5Sham.bam P8_1.5Sham_rep1/P8_1.5Sham_rep1_H3K27me3_uniquely_rm.bam P8_1.5Sham_rep2/P8_1.5Sham_rep2_H3K27me3_uniquely_rm.bam
+samtools index -@ 50 P8_1.5Sham.bam
+bamCompare -b1 ~/ChIP_seq/mouse/P8_heart/H3K27me3/P8_1.5Sham.bam -b2 ~/ChIP_seq/mouse/P8_heart/H3K27ac/P8_1.5Sham_input.bam \
+           --binSize 1 -p max -of bigwig -o P8_1.5Sham_log2ratio.bw
+
+## P8 1.5MI
+samtools merge -@ 50 P8_1.5MI.bam P8_1.5MI_rep1/P8_1.5MI_rep1_H3K27me3_uniquely_rm.bam P8_1.5MI_rep2/P8_1.5MI_rep2_H3K27me3_uniquely_rm.bam
+samtools index -@ 50 P8_1.5MI.bam
+bamCompare -b1 ~/ChIP_seq/mouse/P8_heart/H3K27me3/P8_1.5MI.bam -b2 ~/ChIP_seq/mouse/P8_heart/H3K27ac/P8_1.5MI_input.bam \
+           --binSize 1 -p max -of bigwig -o P8_1.5MI_log2ratio.bw
+
